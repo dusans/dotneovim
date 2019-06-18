750ce29467d78aee0a57d433792e6bf21e81b1cc
dusans <dusan.smitran@gmail.com>
2019-06-18 16:11:55 +0200

    update: update_dependecies.bat and autoload/plug.vim

--- update_dependecies.bat
+++ update_dependecies.bat
@@ -1,15 +1,24 @@
-%PY27_64_CONDA%\Scripts\pip install flake8 --upgrade
+rem ###########################################################
+rem #### PY 27
+rem ###########################################################
 %PY27_64_CONDA%\Scripts\pip install autopep8 --upgrade
-%PY27_64_CONDA%\Scripts\pip install pylint==1.9.4 --upgrade
+%PY27_64_CONDA%\Scripts\pip install flake8 --upgrade
 %PY27_64_CONDA%\Scripts\pip install jedi --upgrade
-%PY27_64_CONDA%\Scripts\pip install ipdb --upgrade
 %PY27_64_CONDA%\Scripts\pip install neovim --upgrade
 %PY27_64_CONDA%\Scripts\pip install pynvim --upgrade
 
-%PY37_64_CONDA%\Scripts\pip install flake8 --upgrade
+rem %PY27_64_CONDA%\Scripts\pip install ipdb --upgrade
+rem %PY27_64_CONDA%\Scripts\pip install pylint==1.9.4 --upgrade
+
+rem ###########################################################
+rem #### PY 37
+rem ###########################################################
 %PY37_64_CONDA%\Scripts\pip install autopep8 --upgrade
-%PY37_64_CONDA%\Scripts\pip install pylint --upgrade
+%PY37_64_CONDA%\Scripts\pip install flake8 --upgrade
 %PY37_64_CONDA%\Scripts\pip install jedi --upgrade
-%PY37_64_CONDA%\Scripts\pip install ipdb --upgrade
 %PY37_64_CONDA%\Scripts\pip install neovim --upgrade
 %PY37_64_CONDA%\Scripts\pip install pynvim --upgrade
+%PY37_64_CONDA%\Scripts\pip install psutil --upgrade
+
+rem %PY37_64_CONDA%\Scripts\pip install ipdb --upgrade
+rem %PY37_64_CONDA%\Scripts\pip install pylint --upgrade
